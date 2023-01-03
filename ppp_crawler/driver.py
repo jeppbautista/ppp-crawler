93e81e9e503d1c39c952ef60c60959b4ec19635a
Jepp Bautista <slapadasbas@gmail.com>
2023-01-03 12:02:50 +0800

    Initial commit

---
+++ ppp_crawler/driver.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+from tiktok_crawler.config import Config
+
+class _Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(_Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+    
+class Driver(metaclass=_Singleton):
+    def __init__(self, *args) -> None:
+        options = Options()
+        for arg in args:
+            options.add_argument(arg)
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    def get_driver(self):
+        return self.driver
+        
---
+++ ppp_crawler/entities.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.remote.webelement import WebElement
+
+from abc import ABC, abstractmethod
+
+class Loans:
+    loan_amount_range: str
+    loan_amount_min: float
+    loan_amount_max: float
+    business_name: str
+    address: str
+    naics_code: str
+    business_type: str
+    race: str
+    gender: str
+    veteran: str
+    jobs_retained: str
+    date_approved: str
+    lender: str
+    cd: str
+    status: str
+    is_extracted: bool = False
+    element: WebElement
+    
+    
+    
